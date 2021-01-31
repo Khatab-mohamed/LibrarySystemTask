0fb2ba47fef93bbebea3c7202b38159510326b35
Dev_Khatab <khatap1@hotmail.com>
2021-01-31 07:58:07 -0800

    Adding Books Controller

---
+++ LibrarySystem/LibrarySystem.Mvc/Controllers/BookController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LibrarySystem.Application.Interfaces;
+using LibrarySystem.Application.ResourceParameter;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LibrarySystem.Mvc.Controllers
+{
+    public class BookController : Controller
+    {
+        private readonly IBooksService _booksService;
+
+        public BookController(IBooksService booksService)
+        {
+            _booksService = booksService;
+        }
+
+        public IActionResult Index([FromBody] BookResourceParameter bookResourceParameter)
+        {
+            var model = _booksService.GetBooks(bookResourceParameter);
+            return View(model);
+        }
+    }
+}
