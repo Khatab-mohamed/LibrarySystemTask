759c8eb4c6775e865a477e432227d844506592fb
Dev_Khatab <khatap1@hotmail.com>
2021-01-31 06:29:34 -0800

    Implement The GetBooks Method, using Linq

--- LibrarySystem/LibrarySystem.Application/Interfaces/IBookService.cs
+++ LibrarySystem/LibrarySystem.Application/Interfaces/IBookService.cs
@@ -1,10 +1,12 @@
 ﻿using LibrarySystem.Domain.Entities;
 using System.Collections.Generic;
+using LibrarySystem.Application.ResourceParameter;
 
 namespace LibrarySystem.Application.Interfaces
 {
     public interface IBooksService
     {
-       
+        IEnumerable<Book> GetBooks(BookResourceParameter parameter);
+
     }
 }
--- LibrarySystem/LibrarySystem.Application/Services/BooksService.cs
+++ LibrarySystem/LibrarySystem.Application/Services/BooksService.cs
@@ -1,9 +1,45 @@
-﻿using LibrarySystem.Application.Interfaces;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using LibrarySystem.Application.Interfaces;
+using LibrarySystem.Application.ResourceParameter;
+using LibrarySystem.Data.Context;
+using LibrarySystem.Domain.Entities;
 
 namespace LibrarySystem.Application.Services
 {
     public class BooksService : IBooksService
     {
+        private readonly LibraryDbContext _context;
+        public BooksService(LibraryDbContext context)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+        private IEnumerable<Book> GetBooks()
+        {
+            return _context.Books.OrderBy(c => c.Name).ToList();
+        }
+
+        public IEnumerable<Book> GetBooks(BookResourceParameter bookResourceParameter)
+        {
+
+            if (string.IsNullOrWhiteSpace(bookResourceParameter.MainCategory) || string.IsNullOrWhiteSpace(bookResourceParameter.SubCategory))
+                return GetBooks();
+            var collection = _context.Books as IQueryable<Book>;
+            if (!string.IsNullOrWhiteSpace(bookResourceParameter.MainCategory))
+            {
+                var mainCategory = bookResourceParameter.MainCategory.Trim();
+                collection = collection.Where(a => a.MainCategory == mainCategory);
+            }
+
+            if (string.IsNullOrWhiteSpace(bookResourceParameter.SubCategory)) return collection.ToList();
+            {
+                var subCategory = bookResourceParameter.SubCategory.Trim();
+                collection = collection.Where(a => a.SubCategory == subCategory);
+            }
+
+            return collection.ToList();
+        }
 
     }
 }
