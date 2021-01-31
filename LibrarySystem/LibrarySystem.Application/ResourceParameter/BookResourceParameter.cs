825fc3453d0af641efffadf0af328baf7c12c675
Dev_Khatab <khatap1@hotmail.com>
2021-01-31 06:28:34 -0800

    Adding The BookResourceParameter

---
+++ LibrarySystem/LibrarySystem.Application/ResourceParameter/BookResourceParameter.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibrarySystem.Application.ResourceParameter
+{
+    public class BookResourceParameter
+    {
+        public string MainCategory { get; set; }
+        public string SubCategory { get; set; }
+    }
+}
