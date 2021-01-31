a7ac5ea9440eeb9585b00a31c9649c4da4b40c59
Dev_Khatab <khatap1@hotmail.com>
2021-01-31 10:57:56 -0800

    Api Controller

---
+++ LibrarySystem/LibrarySystem.Mvc/Controllers/BooksController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LibrarySystem.Application.Interfaces;
+using LibrarySystem.Application.ResourceParameter;
+using LibrarySystem.Domain.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LibrarySystem.Mvc.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private readonly IBooksService _booksService;
+
+        public BooksController(IBooksService booksService)
+        {
+            _booksService = booksService;
+        }
+
+
+        [HttpGet]
+        [HttpHead]
+        public ActionResult<IEnumerable<Book>> GetAuthors(
+            [FromQuery] BookResourceParameter bookResourceParameter)
+        {
+            var books = _booksService.GetBooks(bookResourceParameter);
+
+            return Ok(books);
+        }
+    }
+}
