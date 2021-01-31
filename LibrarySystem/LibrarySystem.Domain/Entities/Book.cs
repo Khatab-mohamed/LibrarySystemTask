56fb831e404e909bfcbe10b4d61edcd50ae46a47
Dev_Khatab <khatap1@hotmail.com>
2021-01-31 05:48:34 -0800

    Adding The Domain Entities

---
+++ LibrarySystem/LibrarySystem.Domain/Entities/Book.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace LibrarySystem.Domain.Entities
+{
+    public class Book
+    {
+
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string MainCategory { get; set; }
+        public string SubCategory { get; set; }
+        public Author Author { get; set; }
+        public Guid AuthorId { get; set; }
+    }
+}
