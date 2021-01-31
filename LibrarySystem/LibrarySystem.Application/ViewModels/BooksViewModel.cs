b185c3c21400d81d7197c3a7d5632eec38180ee8
Dev_Khatab <khatap1@hotmail.com>
2021-01-31 08:05:13 -0800

    Adding BooksViewModel

---
+++ LibrarySystem/LibrarySystem.Application/ViewModels/BooksViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using LibrarySystem.Domain.Entities;
+
+namespace LibrarySystem.Application.ViewModels
+{
+    public class BooksViewModel
+    {
+        public ICollection<Book> Books { get; set; }
+    }
+}
