f161688d02f6c261350edaa8cbfb14750c5d2e61
Dev_Khatab <khatap1@hotmail.com>
2021-01-31 05:55:35 -0800

    Adding LibraryDbContext

---
+++ LibrarySystem/LibrarySystem.Data/Context/LibraryDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using LibrarySystem.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibrarySystem.Data.Context
+{
+    public class LibraryDbContext : DbContext
+    {
+        private readonly DbContextOptions<LibraryDbContext> _options;
+
+        public LibraryDbContext(DbContextOptions<LibraryDbContext> options) : base(options)
+        {
+            _options = options;
+        }
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Author> Authors { get; set; }
+
+    }
+}
