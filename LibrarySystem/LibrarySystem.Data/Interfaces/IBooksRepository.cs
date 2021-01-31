6b53f03ad170a780aecef8ca10eab701a2e8bb0e
Dev_Khatab <khatap1@hotmail.com>
2021-01-31 08:31:17 -0800

    Move the Domain Logic to the Repo

--- LibrarySystem/LibrarySystem.Data/Interfaces/IBooksRepository.cs
+++ LibrarySystem/LibrarySystem.Data/Interfaces/IBooksRepository.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using LibrarySystem.Domain.Entities;
 
 namespace LibrarySystem.Data.Interfaces
 {
     public interface IBooksRepository
     {
+        IEnumerable<Book> GetBooks(string mainCategory, string subCategory);
     }
 }
--- LibrarySystem/LibrarySystem.Data/Repositories/BooksRepository.cs
+++ LibrarySystem/LibrarySystem.Data/Repositories/BooksRepository.cs
@@ -1,10 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using LibrarySystem.Data.Context;
+using LibrarySystem.Domain.Entities;
 
 namespace LibrarySystem.Data.Repositories
 {
     public class BooksRepository
     {
+        private readonly LibraryDbContext _context;
+        public BooksRepository(LibraryDbContext context)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+        private IEnumerable<Book> GetBooks()
+        {
+            return _context.Books.OrderBy(c => c.Name).ToList();
+        }
+
+        public IEnumerable<Book> GetBooks(string _mainCategory, string _subCategory)
+        {
+
+            if (string.IsNullOrWhiteSpace(_mainCategory) || string.IsNullOrWhiteSpace(_subCategory))
+                return GetBooks();
+            var collection = _context.Books as IQueryable<Book>;
+            if (!string.IsNullOrWhiteSpace(_mainCategory))
+            {
+                var mainCategory = _mainCategory.Trim();
+                collection = collection.Where(a => a.MainCategory == mainCategory);
+            }
+
+            if (string.IsNullOrWhiteSpace(_mainCategory)) return collection.ToList();
+            {
+                var subCategory = _subCategory.Trim();
+                collection = collection.Where(a => a.SubCategory == subCategory);
+            }
+
+            return collection.ToList();
+        }
+
     }
 }
